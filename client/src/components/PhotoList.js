8fe3d496ee9a369d1e37b722841cd28c266c26c1
Albert Zuo <albert.zuo@gmail.com>
2017-04-01 21:17:28 -0400

    Skeleton with working upvote downvote

--- client/src/components/PhotoList.js
+++ client/src/components/PhotoList.js
@@ -5,27 +5,28 @@ import PhotoDetail from './PhotoDetail';
 import { footerSize } from './common/Footer';
 
 class PhotoList extends Component {
-    state = { photos: [] }
+  state = { photos: [], restaurants: [] }
 
-    componentWillMount() {
-        axios.get('https://fotafood.herokuapp.com/api/photo?lat=55.1234&lng=-123.551') //  https://rallycoding.herokuapp.com/api/music_albums
-            .then(response => this.setState({ photos: response.data }));
-    }
+  componentWillMount() {
+      axios.get('https://fotafood.herokuapp.com/api/photo?order=hot&lat=55.1234&lng=-123.551')
+          .then(response => this.setState({ photos: response.data.photos,
+                                            restaurants: response.data.restaurants }));
+  }
 
-    renderPhotos() {
-        return this.state.photos.map(photo =>
-            <PhotoDetail key={photo.id} photo={photo} />
-            // Later on key should be id of user who uploaded it
-        );
-    }
+  renderPhotos() {
+      return this.state.photos.map(photo =>
+          <PhotoDetail key={photo.id} photo={photo} />
+          // Later on key should be id of user who uploaded it
+      );
+  }
 
-    render() {
-        return (
-            <ScrollView style={{ marginBottom: footerSize }}>
-                {this.renderPhotos()}
-            </ScrollView>
-        );
-    }
+  render() {
+      return (
+          <ScrollView style={{ marginBottom: footerSize }}>
+              {this.renderPhotos()}
+          </ScrollView>
+      );
+  }
 }
 
 export default PhotoList;
