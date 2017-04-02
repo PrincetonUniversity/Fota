8e5b06601a3b69965b1b189748e8b4d7cb69cca0
Albert Zuo <albert.zuo@gmail.com>
2017-04-01 22:04:17 -0400

    Fully working upvote/downvote

--- client/src/components/PhotoDetail.js
+++ client/src/components/PhotoDetail.js
@@ -3,37 +3,37 @@ import { View, Image, Text } from 'react-native';
 import { Card, CardSection, ImageButton } from './common';
 
 const styles = {
-    photoStyle: { // The picture
-        height: 300,
-        flex: 1,
-        width: null
-    },
-    voteStyle: { // Upvote/downvote
-        height: 25,
-        width: 25
-    },
-    likeCountContainerStyle: { // Upvote arrow + number of likes container
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginLeft: 10
-    },
-    likeCountArrowStyle: { // the arrow next to number of likes
-        height: 15,
-        width: 15
-    },
-    likeCountTextStyle: { // Number of likes
-        fontSize: 15,
-        textAlign: 'justify',
-        fontWeight: 'bold',
-        marginLeft: 5
-    },
-    likeContainerStyle: { // Upvote/downvote container
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'stretch',
-        marginRight: 10
-    },
+  photoStyle: { // The picture
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  voteStyle: { // Upvote/downvote
+    height: 25,
+    width: 25
+  },
+  likeCountContainerStyle: { // Upvote arrow + number of likes container
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 10
+  },
+  likeCountArrowStyle: { // the arrow next to number of likes
+    height: 15,
+    width: 15
+  },
+  likeCountTextStyle: { // Number of likes
+    fontSize: 15,
+    textAlign: 'justify',
+    fontWeight: 'bold',
+    marginLeft: 5
+  },
+  likeContainerStyle: { // Upvote/downvote container
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    marginRight: 10
+  },
 };
 
 const { photoStyle,
@@ -44,6 +44,11 @@ const { photoStyle,
         likeContainerStyle
      } = styles;
 
+const upvoteUnactivated = require('../img/upvote_unactivated.png');
+const upvoteActivated = require('../img/upvote_activated.png');
+const downvoteUnactivated = require('../img/downvote_unactivated.png');
+const downvoteActivated = require('../img/downvote_activated.png');
+
 class PhotoDetail extends Component {
   constructor(props) {
     super(props);
@@ -51,16 +56,66 @@ class PhotoDetail extends Component {
                     likecount: props.photo.likecount,
                     userLiked: false,
                     userDisliked: false,
-                    upvoteSource: require('../img/upvote_unactivated.png'),
-                    downvoteSource: require('../img/downvote_unactivated.png'),
+                    upvoteSource: upvoteUnactivated,
+                    downvoteSource: downvoteUnactivated,
                     userHasVoted: false
                   };
   }
+
+  renderUpvote() {
+    let newLikeCount = this.state.likecount;
+    let userNewLike = true;
+    let userVoted = true;
+    let voteSource = upvoteActivated;
+    if (!this.state.userHasVoted) {
+      newLikeCount += 1;
+    } else if (this.state.userDisliked) {
+        newLikeCount += 2;
+    } else if (this.state.userLiked) {
+        newLikeCount -= 1;
+        userNewLike = false;
+        userVoted = false;
+        voteSource = upvoteUnactivated;
+    }
+    this.setState({ likecount: newLikeCount,
+                    userLiked: userNewLike,
+                    userDisliked: false,
+                    userHasVoted: userVoted,
+                    upvoteSource: voteSource,
+                    downvoteSource: downvoteUnactivated
+                  });
+  }
+
+  renderDownvote() {
+    let newLikeCount = this.state.likecount;
+    let userHasDisliked = true;
+    let userVoted = true;
+    let voteSource = downvoteActivated;
+    if (!this.state.userHasVoted) {
+      newLikeCount -= 1;
+    } else if (this.state.userLiked) {
+      newLikeCount -= 2;
+    } else if (this.state.userDisliked) {
+      newLikeCount += 1;
+      userHasDisliked = false;
+      userVoted = false;
+      voteSource = downvoteUnactivated;
+    }
+    this.setState({ likecount: newLikeCount,
+                    userLiked: false,
+                    userDisliked: userHasDisliked,
+                    userHasVoted: userVoted,
+                    upvoteSource: upvoteUnactivated,
+                    downvoteSource: voteSource
+                  });
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
             <ImageButton
+                activeOpacity={1}
                 style={photoStyle}
                 source={{ uri: this.state.link }}
                 onPress={() => console.log('pressed')}
@@ -80,42 +135,12 @@ class PhotoDetail extends Component {
             <ImageButton
                 source={this.state.upvoteSource}
                 style={voteStyle}
-                onPress={() => {
-                  var newLikeCount = this.state.likecount;
-                  if (!this.state.userHasVoted) {
-                    newLikeCount += 1;
-                  } else if (this.state.userDisliked) {
-                      newLikeCount += 2;
-                  }
-                  this.setState({ likecount: newLikeCount,
-                                  userLiked: true,
-                                  userDisliked: false,
-                                  userHasVoted: true,
-                                  upvoteSource: require('../img/upvote_activated.png'),
-                                  downvoteSource: require('../img/downvote_unactivated.png')
-                                });
-                  }
-                }
+                onPress={() => this.renderUpvote()}
             />
             <ImageButton
                 source={this.state.downvoteSource}
                 style={voteStyle}
-                onPress={() => {
-                  var newLikeCount = this.state.likecount;
-                  if (!this.state.userHasVoted) {
-                    newLikeCount -= 1;
-                  } else if (this.state.userLiked) {
-                      newLikeCount -= 2;
-                  }
-                  this.setState({ likecount: newLikeCount,
-                                  userLiked: false,
-                                  userDisliked: true,
-                                  userHasVoted: true,
-                                  upvoteSource: require('../img/upvote_unactivated.png'),
-                                  downvoteSource: require('../img/downvote_activated.png')
-                                });
-                  }
-                }
+                onPress={() => this.renderDownvote()}
             />
           </View>
         </CardSection>
--- client/src/components/common/ImageButton.js
+++ client/src/components/common/ImageButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Image, TouchableOpacity } from 'react-native';
 
-const ImageButton = ({ style, onPress, source }) => (
+const ImageButton = ({ style, onPress, source, activeOpacity }) => (
   <TouchableOpacity
     style={{ width: style.width, height: style.height, flex: style.flex }}
     onPress={onPress}
+    activeOpacity={activeOpacity}
   >
     <Image
        style={{ width: style.width, height: style.height, resizeMode: 'stretch' }}
