301a9ae055f50bbf2b7c8f677f23a0be76f47159
Bryan Zhu <bwzhu@princeton.edu>
2017-04-14 16:57:47 -0400

    basic search page

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Header, Input } from './common';
+import { Text, View, ListView } from 'react-native';
+import axios from 'axios';
+import { Header, Input, CardSection } from './common';
 
 class SearchPage extends Component {
   state = { query: '', rlist: [], totalList: [] }
 
-  renderSearchResults() {
-    this.state.query
+  componentWillMount() {
+    axios.get('https://fotafood.herokuapp.com/api/restaurant')
+      .then(response => this.setState({ totalList: response.data }));
+  }
+
+  updateQuery(query) {
+    let rlist = [];
+    if (query !== '') {
+      rlist = this.state.totalList.filter(restaurant =>
+        restaurant.name.toLowerCase().includes(query.toLowerCase())
+      );
+    }
+    this.setState({ query, rlist });
+  }
+
+  renderRestaurant(restaurant) {
+    return (
+      <CardSection>
+        <Text>{restaurant.name}</Text>
+      </CardSection>
+    );
   }
 
   render() {
+    const dataSource = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1.id !== r2.id
+        });
     return (
       <View>
         <Header>
@@ -17,10 +40,14 @@ class SearchPage extends Component {
             label={require('../img/magnifying_glass_unactivated.png')}
             placeholder='Search'
             value={this.state.query}
-            onChangeText={query => this.setState({ query })}
+            onChangeText={query => this.updateQuery(query)}
           />
        </Header>
-       {this.renderSearchResults()}
+       <ListView
+         dataSource={dataSource.cloneWithRows(this.state.rlist)}
+         renderRow={restaurant => this.renderRestaurant(restaurant)}
+         enableEmptySections
+       />
      </View>
     );
   }
