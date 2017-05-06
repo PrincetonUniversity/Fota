5bd1c4203d760508e3e3c002a7fa96c60e10c61b
Jay Lee <junghwan@princeton.edu>
2017-05-06 14:37:43 -0400

    Finished server comment sorting

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -27,6 +27,27 @@ module.exports.post = (req, res) => {
   })
 }
 
+function getAdjArray(comments, noun) {
+  totalCount = 0;
+  adjectives = [];
+  comments.forEach((comment) => {
+    if (comment.noun === noun) {
+      adjectives.push({ word: comment.adj, count: comment.count });
+      totalCount += comment.count;
+    }
+  });
+
+  // Numbers in comparator below switched intentionally
+  // This allows for descending sorting
+  adj = adjectives.sort((a1, a2) => {
+    if (a1.count >= a2.count) return -1;
+    if (a1.count <= a2.count) return 1;
+    else return 0;
+  });
+
+  return { adj, totalCount }
+}
+
 // retrieve all comments, given a valid restaurant ID.
 module.exports.get = (req, res) => {
   const id = req.params.restaurantId;
@@ -38,7 +59,31 @@ module.exports.get = (req, res) => {
       return res.status(404).send({error: "Restaurant with given id does not exist"});
 
     rest.getComments().then((comments) => {
-      return res.status(200).send(comments);
+      let nouns = [];
+      // First get all the nouns
+      comments.forEach((comment) => {
+        if (!nouns.includes(comment.noun))
+          nouns.push(comment.noun);
+      });
+
+      // Compile an array of objects
+      let result = [];
+      nouns.forEach((noun) => {
+        const { adj, totalCount } = (getAdjArray(comments, noun));
+        result.push({
+          noun,
+          adj,
+          totalCount
+        });
+      });
+
+      const sortedResult = result.sort((a1, a2) => {
+        if (a1.totalCount >= a2.totalCount) return -1;
+        if (a1.totalCount <= a2.totalCount) return 1;
+        else return 0;
+      })
+
+      return res.status(200).send(sortedResult);
     });
   }).catch((e) => {
     console.log(e);
