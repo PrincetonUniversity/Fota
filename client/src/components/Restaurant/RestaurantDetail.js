090f669309e8ef23a61af237788eaeacb3268dfb
Albert Zuo <albert.zuo@gmail.com>
2017-05-07 14:10:31 -0400

    bwzhu2

--- client/src/components/Restaurant/RestaurantDetail.js
+++ client/src/components/Restaurant/RestaurantDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image, Text, ScrollView, FlatList } from 'react-native';
+import { View, Image, Text, ScrollView, FlatList, TouchableOpacity, Navigator } from 'react-native';
 import moment from 'moment';
 import { phonecall } from 'react-native-communications';
 import request from '../../helpers/axioshelper';
@@ -101,7 +101,7 @@ class RestaurantDetail extends Component {
       .then(res2 => this.setState({
         photos: response.data,
         loading: false,
-        comments: res2.data
+        nouns: res2.data
       })).catch(e => request.showErrorAlert(e));
     }).catch(e => request.showErrorAlert(e));
   }
@@ -249,21 +249,58 @@ class RestaurantDetail extends Component {
     return (
       <FlatList
         data={this.state.nouns}
-        keyExtractor={noun => noun}
-        numColumns={2}
-        renderItem={noun => this.renderNoun(noun)}
+        keyExtractor={noun => `${noun.adj} + ${noun.noun}`}
+        // numColumns={1}
+        renderItem={noun => this.renderNoun(noun.item)}
         bounces={false}
       />
     );
   }
 
+  // renderCommentScene(route, navigator) {
+  //   switch (route.id) {
+  //     case 0:
+  //       return (
+  //         <FlatList
+  //           data={this.state.nouns}
+  //           keyExtractor={noun => `${noun.adj} + ${noun.noun}`}
+  //           renderItem={noun => this.renderNoun(noun.item, navigator)}
+  //           bounces={false}
+  //         />
+  //       );
+  //     case 1:
+  //       return (
+  //         <View>
+  //           <Text onPress={this.setState({ noun: null })}>Back</Text>
+  //           <NounDetail noun={this.state.noun} />
+  //         </View>
+  //       );
+  //     default:
+  //       return (
+  //         <FlatList
+  //           data={this.state.nouns}
+  //           keyExtractor={noun => `${noun.adj} + ${noun.noun}`}
+  //           renderItem={noun => this.renderNoun(noun.item, navigator)}
+  //           bounces={false}
+  //         />
+  //       );
+  //   }
+  // }
+
+  // updateNoun(noun) {
+  //   this.setState({ noun });
+  //   console.log(this.state.noun);
+  // }
+
   renderNoun(noun) {
     const commentString = `${noun.adj[0].word} ${noun.noun}: ${noun.adj[0].count}`;
     return (
-      <CommentDisplay
-        key={commentString}
-        text={commentString}
-      />
+      <TouchableOpacity onPress={() => this.setState({ noun })}>
+        <CommentDisplay
+          key={commentString}
+          text={commentString}
+        />
+      </TouchableOpacity>
     );
   }
 
--- client/src/components/Settings/SettingsPage.js
+++ client/src/components/Settings/SettingsPage.js
@@ -1,36 +1,78 @@
 import React, { Component } from 'react';
-import { Text, View, AsyncStorage, TouchableOpacity } from 'react-native';
+import { Text, View, AsyncStorage, TouchableHighlight, Image, FlatList } from 'react-native';
 import { Header } from '../common';
 
 const radii = [1, 5, 10, 25];
+const checkmark = require('../../img/fota_home_activated.png');
 
 class SettingsPage extends Component {
-  renderRadii() {
-    console.log('here');
-    return radii.map(radius =>
-      <TouchableOpacity key={radius} onPress={() => AsyncStorage.setItem('SearchRadius', radius.toString())}>
-        <View style={{ flexDirection: 'row', marginTop: 5, marginBottom: 5 }}>
+  state = { radius: '', fetchingComplete: false }
+
+  componentWillMount() {
+    AsyncStorage.getItem('SearchRadius').then(currentRadius =>
+      this.setState({ radius: parseInt(currentRadius, 10), fetchingComplete: true })
+    );
+  }
+
+  componentDidUpdate() {
+    AsyncStorage.getItem('SearchRadius').then(currentRadius =>
+      this.setState({ radius: parseInt(currentRadius, 10) })
+    );
+  }
+
+  renderCheckmark(radius) {
+    if (radius === this.state.radius) {
+      return (
+        <Image
+          source={checkmark}
+          style={{ width: 15, height: 15 }}
+        />
+      );
+    }
+    return;
+  }
+
+  renderRadius(radius) {
+    return (
+      <TouchableHighlight
+        key={radius}
+        onPress={() => {
+          this.setState({ radius });
+          AsyncStorage.setItem('SearchRadius', radius.toString());
+        }}
+      >
+        <View style={styles.distanceStyle}>
           <Text style={{ fontSize: 15, fontFamily: 'Avenir' }}>
             {radius} mi.
           </Text>
+          {this.renderCheckmark(radius)}
         </View>
-      </TouchableOpacity>
+      </TouchableHighlight>
     );
   }
 
   render() {
-    return (
-      <View>
-        <Header>
-          <Text style={styles.headerTextStyle}>Settings</Text>
-        </Header>
+    if (this.state.fetchingComplete) {
+      return (
         <View>
-          <Text style={{ fontSize: 17, fontFamily: 'Avenir' }}>
-            Distance Away
-          </Text>
-          {this.renderRadii()}
+          <Header>
+            <Text style={styles.headerTextStyle}>Settings</Text>
+          </Header>
+          <View>
+            <Text style={{ fontSize: 17, fontFamily: 'Avenir' }}>
+              Distance Away
+            </Text>
+            <FlatList
+              data={radii}
+              keyExtractor={index => index}
+              renderItem={radius => this.renderRadius(radius.item)}
+            />
+          </View>
         </View>
-      </View>
+      );
+    }
+    return (
+      <View />
     );
   }
 }
@@ -42,6 +84,13 @@ const styles = {
     fontFamily: 'Avenir',
     fontSize: 20,
     color: '#000'
+  },
+  distanceStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 5,
+    marginBottom: 5
   }
 };
 
