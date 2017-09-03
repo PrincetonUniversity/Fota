9b3330496787ab9ca725f9154414b3de13ead5d5
Bryan Zhu <bwzhu@princeton.edu>
2017-09-03 18:06:50 -0400

    removed some console logs

--- client/src/components/Restaurant/LoadingRestaurants.js
+++ client/src/components/Restaurant/LoadingRestaurants.js
@@ -5,11 +5,7 @@ const LoadingRestaurants = () => (
   <View style={{ flex: 1, backgroundColor: 'white' }}>
     <StatusBar animated barStyle='light-content' />
     <View style={{ height: 175, backgroundColor: '#d3d3d3' }} />
-    <View style={{ elevation: 1, backgroundColor: '#ffffff' }} >
-      <View style={{ height: 84.5, backgroundColor: 'white' }} />
-      <View style={styles.infoContainerStyle} />
-      <View style={styles.tabBarStyle} />
-    </View>
+    <View style={styles.tabBarStyle} />
     <View style={{ flex: 1 }} />
     <View style={styles.footerStyle}>
       {/* <View style={[styles.bottomSpacerStyle, { borderRightWidth: 1 }]} /> */}
@@ -19,17 +15,17 @@ const LoadingRestaurants = () => (
 );
 
 const styles = {
-  infoContainerStyle: {
-    height: 73.5,
-    backgroundColor: 'white',
-    zIndex: 4,
-    marginHorizontal: 30,
-    //borderTopWidth: 1,
-    //borderBottomWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.2)'
-  },
+  // infoContainerStyle: {
+  //   height: 73.5,
+  //   backgroundColor: 'white',
+  //   zIndex: 4,
+  //   marginHorizontal: 30,
+  //   //borderTopWidth: 1,
+  //   //borderBottomWidth: 1,
+  //   borderColor: 'rgba(0, 0, 0, 0.2)'
+  // },
   tabBarStyle: {
-    height: 45,
+    height: 203,
     backgroundColor: 'white',
     shadowOpacity: 0.08,
     shadowRadius: 3,
